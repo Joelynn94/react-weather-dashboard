bb5cbab5a73ecf77ff56e2509080828ca0c92943
Joseph Lynn <josephlynn@Sierras-MacBook-Pro.local>
2020-10-17 23:35:26 -0400

    passing array to weather component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ import Weather from './components/Weather'
 
 function App() {
 
-  const [results, setReults] = useState({})
+  const [results, setReults] = useState({
+    cityName: '',
+    country: '',
+    items: []
+  })
   const [query, setQuery] = useState('')
 
   useEffect(() => {
@@ -15,25 +19,36 @@ function App() {
   }, [query])
 
   const fetchWeather = async () => {
+    // const URL = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${query}&cnt=7&appid=${process.env.REACT_APP_API_KEY}`
     const URL = `https://api.openweathermap.org/data/2.5/forecast?q=${query}&appid=${process.env.REACT_APP_API_KEY}`
   
     const response = await axios.get(URL, {
       params: {
-        q: query,
         units: 'imperial',
-        appid: process.env.REACT_APP_API_KEY
       }
     })
   
-    console.log(response.data)
-    setReults(response)
+    const objToArray = [...Object.entries(response.data.list)]
+    console.log(typeof objToArray, objToArray)
+
+    console.log({
+      cityName: response.data.city.name,
+      country: response.data.city.country,
+      items: response.data.list
+    })
+    setReults({
+      cityName: response.data.city.name,
+      country: response.data.city.country,
+      items: response.data.list
+    })
+
   }
 
 
   return (
     <div>
       <Search getQuerySearch={(search) => setQuery(search)}/>
-      <Weather city={results}/>
+      <Weather cityName={results.cityName} country={results.country} result={results} />
     </div>
   );
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
-function Weather({city}) {
+function Weather({cityName, country, result}) {
+
+  const arrayResults = result.items
+  console.log(typeof arrayResults, arrayResults)
+
   return (
     <div>
-      <h2>{city.name}</h2>
-      <h5>Country</h5>
+      <h2>{cityName}</h2>
+      <h5>{country}</h5>
+
     </div>
   )
 }
